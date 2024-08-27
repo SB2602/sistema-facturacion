878664e3e540eaa3116f084442ae602ce292de96
Sair Samuel <0512020011@alumnos.unp.edu.pe>
2024-08-27 00:29:02 -0500

    brayan

---
+++ src/app/brayan/brayan.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brayan',
+  standalone: true,
+  imports: [],
+  templateUrl: './brayan.component.html',
+  styleUrl: './brayan.component.css'
+})
+export class BrayanComponent {
+
+}
