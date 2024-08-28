fc54a23798b0a30c7bd25789d969e1ce7fbfd1f2
Sair Samuel <0512020011@alumnos.unp.edu.pe>
2024-08-27 22:17:28 -0500

    create-user

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    CommonModule
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
+  dashboardItems = [
+    { label: 'Productos', icon: 'inventory', link: '/productos' },
+    { label: 'Facturas', icon: 'receipt', link: '/facturas' },
+    { label: 'Proveedores', icon: 'business', link: '/proveedores' },
+    { label: 'Clientes', icon: 'people', link: '/clientes' },
+    { label: 'Usuarios', icon: 'account_circle', link: '/usuarios' },
+  ];
+
+  constructor(private router: Router) {}
+
+  navigateTo(link: string): void {
+    this.router.navigate([link]);
+  }
 }
+
