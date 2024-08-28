b23843fd2a615bfa486eb12221b2b0f0c7aa2564
Sair Samuel <0512020011@alumnos.unp.edu.pe>
2024-08-28 15:32:20 -0500

    user-add

--- src/app/componentes/user/user-add/user-add.component.ts
+++ src/app/componentes/user/user-add/user-add.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
+
+import { Role } from '../../../interfaces/role';
 
 @Component({
   selector: 'app-user-add',
   standalone: true,
-  imports: [],
   templateUrl: './user-add.component.html',
-  styleUrl: './user-add.component.css'
+  styleUrl: './user-add.component.css',
+  imports: [
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserAddComponent {
-
+  nameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^[a-zA-Z ]+$'),
+  ]);
+  surnameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^[a-zA-Z ]+$'),
+  ]);
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.min(5),
+    Validators.pattern('^[a-zA-Z ]+$'),
+  ]);
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+  roleControl = new FormControl<Role | null>(null, Validators.required);
+  roles: Role[] = [{ name: 'Administrador' }, { name: 'Vendedor' }];
 }
