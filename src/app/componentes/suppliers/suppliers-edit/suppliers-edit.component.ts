7243ef84a36eb0b7889cdfa843fc18d2a242adef
Sair Samuel <0512020011@alumnos.unp.edu.pe>
2024-08-27 23:48:17 -0500

    cree-proveedores

---
+++ src/app/componentes/suppliers/suppliers-edit/suppliers-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suppliers-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './suppliers-edit.component.html',
+  styleUrl: './suppliers-edit.component.css'
+})
+export class SuppliersEditComponent {
+
+}
