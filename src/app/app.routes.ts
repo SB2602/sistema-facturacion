4a54f19f72876f8ac08d9c79189c3a993c0f4592
Sair Samuel <0512020011@alumnos.unp.edu.pe>
2024-08-27 20:10:15 -0500

    reubicacion-carpetas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
