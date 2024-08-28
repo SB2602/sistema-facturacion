426b9cb910f423d38fcc13d51e43daa730cd9216
BrayanRN1114 <0512020022@alumnos.unp.edu.pe>
2024-08-28 15:47:38 -0500

    Add Categories&Products

--- src/app/componentes/categories/categories-add/categories-add.component.ts
+++ src/app/componentes/categories/categories-add/categories-add.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
+
+import { Role } from '../../../interfaces/role';
 
 @Component({
   selector: 'app-categories-add',
   standalone: true,
-  imports: [],
   templateUrl: './categories-add.component.html',
-  styleUrl: './categories-add.component.css'
+  styleUrl: './categories-add.component.css',
+  imports: [
+    FormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CategoriesAddComponent {
-
+  nameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^[a-zA-Z ]+$'),
+  ]);
+  surnameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^[a-zA-Z ]+$'),
+  ]);
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.min(5),
+    Validators.pattern('^[a-zA-Z ]+$'),
+  ]);
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+  roleControl = new FormControl<Role | null>(null, Validators.required);
+  roles: Role[] = [{ name: 'Administrador' }, { name: 'Vendedor' }];
 }
